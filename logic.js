68ba003c63d611baf8acd5a2d6c8432ded68462b
Weisaw <meysamm74@gmail.com>
2022-05-22 12:18:10 +0430

    animation bug fixed

--- logic.js
+++ logic.js
@@ -2,10 +2,10 @@ function date_func() {
     document.getElementById("demo").innerHTML = Date()
 };
 
-$.ajax({
-    type: "POST",
-    url: "~/qrcodegen.py",
-    data: { param: text}
-  }).done(function( o ) {
-     // do something
-  });
+// $.ajax({
+//     type: "POST",
+//     url: "~/qrcodegen.py",
+//     data: { param: text}
+//   }).done(function( o ) {
+//      // do something
+//   });
