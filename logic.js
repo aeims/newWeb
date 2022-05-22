6c609ba0a242ca33c282448f6c871224da9ccfd9
Weisaw <meysamm74@gmail.com>
2022-05-22 11:43:23 +0430

    animation and fonts appended

--- logic.js
+++ logic.js
@@ -1,3 +1,11 @@
 function date_func() {
     document.getElementById("demo").innerHTML = Date()
 };
+
+$.ajax({
+    type: "POST",
+    url: "~/qrcodegen.py",
+    data: { param: text}
+  }).done(function( o ) {
+     // do something
+  });
